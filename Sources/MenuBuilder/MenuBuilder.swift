02bb0f44d1dc6bf757d1112473a8d9880ac02e2b
Andrey Chernoprudov <dinloq@gmail.com>
2021-07-27 13:07:08 +0500

    Submenu

--- Sources/MenuBuilder/MenuBuilder.swift
+++ Sources/MenuBuilder/MenuBuilder.swift
@@ -1,8 +1,19 @@
 import UIKit
 
 extension UIMenu {
-    convenience init(@MenuElementsBuilder builder: () -> [UIMenuElement]) {
-        let children = builder()
-        self.init(children: children)
+    convenience init(
+        title: String = "",
+        image: UIImage? = nil,
+        identifier: UIMenu.Identifier? = nil,
+        options: UIMenu.Options = [],
+        @MenuElementsBuilder builder: () -> [UIMenuElement]
+    ) {
+        self.init(
+            title: title,
+            image: image,
+            identifier: identifier,
+            options: options,
+            children: builder()
+        )
     }
 }
--- Tests/MenuBuilderTests/MenuBuilderTests.swift
+++ Tests/MenuBuilderTests/MenuBuilderTests.swift
@@ -3,12 +3,29 @@ import XCTest
 
 final class MenuBuilderTests: XCTestCase {
     func test_buildMenu_withActions() {
+        // actions
         let menu = UIMenu {
             UIAction("Foo", icon: "eye") { _ in }
             UIAction("Bar", icon: "eye") { _ in }
         }
         
+        // assertions
         XCTAssertEqual(menu.children[0].title, "Foo")
         XCTAssertEqual(menu.children[1].title, "Bar")
+    }
+    
+    func test_buildSubMenu_withTitle() {
+        // actions
+        let menu = UIMenu {
+            UIMenu(title: "Submenu") {
+                UIAction("Bar", icon: "eye") { _ in }
+            }
+        }
+        
+        // assertions
+        let submenu = menu.children[0] as? UIMenu
+        XCTAssertNotNil(submenu)
+        
+        XCTAssertEqual(submenu?.children[0].title, "Bar")
     }
 }
