a33240bb7e4cc9f03e1d66da8e4ed6f6e1ce593a
Andrey Chernoprudov <dinloq@gmail.com>
2021-07-27 13:16:19 +0500

    if else conditional tests

--- Tests/MenuBuilderTests/MenuBuilderTests.swift
+++ Tests/MenuBuilderTests/MenuBuilderTests.swift
@@ -60,4 +60,40 @@ final class MenuBuilderTests: XCTestCase {
         XCTAssertNotNil(menu.children[safe: 0])
         XCTAssertNil(menu.children[safe: 1])
     }
+    
+    func test_buildMenu_ifElseConditional_ifBranch() {
+        // actions
+        let addFooItem = true
+        
+        let menu = UIMenu {
+            
+            if addFooItem {
+                UIAction("Foo", icon: "eye") { _ in }
+            } else {
+                UIAction("Bar", icon: "eye") { _ in }
+            }
+        }
+        
+        // assertions
+        XCTAssertEqual(menu.children[safe: 0]?.title, "Foo")
+        XCTAssertEqual(menu.children.count, 1)
+    }
+    
+    func test_buildMenu_ifElseConditional_elseBranch() {
+        // actions
+        let addFooItem = false
+        
+        let menu = UIMenu {
+            
+            if addFooItem {
+                UIAction("Foo", icon: "eye") { _ in }
+            } else {
+                UIAction("Bar", icon: "eye") { _ in }
+            }
+        }
+        
+        // assertions
+        XCTAssertEqual(menu.children[safe: 0]?.title, "Else")
+        XCTAssertEqual(menu.children.count, 1)
+    }
 }
