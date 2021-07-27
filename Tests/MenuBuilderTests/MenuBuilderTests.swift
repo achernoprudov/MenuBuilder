f770b90da07aa425a5c16808f0c81f3a88b52b0c
Andrey Chernoprudov <dinloq@gmail.com>
2021-07-27 12:31:52 +0500

    MenuBuilder init

---
+++ Tests/MenuBuilderTests/MenuBuilderTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import MenuBuilder
+
+    final class MenuBuilderTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(MenuBuilder().text, "Hello, World!")
+        }
+    }
